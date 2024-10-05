8069720096e67f833592002cbcf5435096d1b90b
Aman Kumar Singh <theprodigalson1712@gmail.com>
2024-10-05 18:18:06 +0530

    added merge function in count inversions

--- arrays/CountInversions.cpp
+++ arrays/CountInversions.cpp
@@ -2,7 +2,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int mergeSort(vector<int> & arr, int low, int high){
+int merge(vector<int> &arr, int low, int mid, int high){
+    int count = 0;
+    int left = low, right = mid+1;
+    vector<int> temp;
+    while(left<=mid && right<=high){
+        if(arr[left]<arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            count += mid - left + 1;
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high)
+        temp.push_back(arr[right++]);
+
+    return count;
+}
+
+int mergeSort(vector<int> &arr, int low, int high){
     int count = 0;
     if(low>=high)
         return count;
