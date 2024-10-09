ca02f50a86dd693701dc9d41ce716013ddd05f12
Aman Kumar Singh <theprodigalson1712@gmail.com>
2024-10-09 15:06:38 +0530

    added four sum using brute force approach

---
+++ arrays/FourSumBrute.cpp
@@ -0,0 +1,27 @@
+// 4-sum using Brute force
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int> &nums, int target){
+    int n = nums.size();
+    set<vector<int>> st;
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n; j++){
+            for(int k = j+1; k<n; k++){
+                for(int l = k+1; l<n; l++){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> res{st.begin(), st.end()};
+    return res;
+}
